f378566bb8a10a7661745397bc4e0b08da162f61
Ngi Bujri <nbujri7@gmail.com>
2023-02-11 20:51:21 -0500

    install mongoose and create Customer model

---
+++ models/customer.js
@@ -0,0 +1,17 @@
+/*
+title: customer.js
+author: ngi bujri
+date: february 11 2023
+description: customer model
+*/
+
+"use strict";
+
+// const mongoose = require("mongoose");
+
+let Customer = new mongoose.Schema({
+  customerID: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+});
+
+module.exports = mongoose.model("Customer", Customer);
